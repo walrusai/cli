6dd3168be5f99b58f9188fff2a220f5f982f6bf4
Akshay Nathan <akshay@walrus.ai>
2020-03-03 12:21:35 -0800

    Handle 402 from runner

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -5,69 +5,58 @@ import logger from "../logger";
 
 const WALRUS_API = "https://api.walrus.ai";
 
+async function pollTest(test: WalrusTest, authToken: string): Promise<WalrusTestExecution> {
+  return new Promise(resolve => {
+    const interval = setInterval(async () => {
+      try {
+        const response = await axios.get(`${WALRUS_API}/${test.gid}`, {
+          headers: { "X-Walrus-Token": authToken },
+        });
+
+        if (response.status === 202) {
+          logger.debug(`Walrus Test ${test.gid} still pending.`);
+        } else {
+          logger.debug(`Walrus Test ${test.gid} completed.`);
+
+          if (timeout) {
+            clearTimeout(timeout);
+          }
+
+          clearInterval(interval);
+          resolve({ name: test.name, ...response.data });
+        }
+      } catch (e) {
+        logger.warn(`Polling Walrus Test ${test.gid} failed: ${e}`);
+      }
+    }, 5000);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      resolve({
+        name: test.name!,
+        success: false,
+        error: `Walrus Test ${test.gid} timed out.`
+      });
+    }, 3600000);
+  });
+}
+
 export async function runTests(
   tests: WalrusTest[],
   authToken: string
 ): Promise<WalrusTestExecution[]> {
-  const starts: WalrusTest[] = await Promise.all(
-    tests.map(async test => {
-      try {
-        const response = await axios.post(
-          WALRUS_API,
-          { ...test, options: { mode: "poll" } },
-          { headers: { "X-Walrus-Token": authToken } }
-        );
+  return await Promise.all(tests.map(async (test) => {
+    try {
+      const result = await axios.post(WALRUS_API, { ...test, options: { mode: "poll" } }, { headers: { "X-Walrus-Token": authToken } });
+      return await pollTest({ ...test, gid: result.data.data.gid, state: result.data.data.state }, authToken);
+    } catch (e) {
+      let error = e.message;
 
-        return {
-          ...test,
-          gid: response.data.data.gid,
-          state: response.data.data.state
-        };
-      } catch (error) {
-        logger.warn(error);
-
-        return { ...test, error: error.response.data.error };
+      if (e.response && e.response.data.error) {
+        error = e.response.data.error;
       }
-    })
-  );
 
-  return await Promise.all(
-    starts.map(
-      (test): Promise<WalrusTestExecution> => {
-        return new Promise(resolve => {
-          const interval = setInterval(async () => {
-            try {
-              const response = await axios.get(`${WALRUS_API}/${test.gid}`, {
-                headers: { "X-Walrus-Token": authToken },
-              });
-
-              if (response.status === 202) {
-                logger.debug(`Walrus Test ${test.gid} still pending.`);
-              } else {
-                logger.debug(`Walrus Test ${test.gid} completed.`);
-
-                if (timeout) {
-                  clearTimeout(timeout);
-                }
-
-                clearInterval(interval);
-                resolve({ name: test.name, ...response.data });
-              }
-            } catch (e) {
-              logger.warn(`Polling Walrus Test ${test.gid} failed: ${e}`);
-            }
-          }, 5000);
-
-          const timeout = setTimeout(() => {
-            clearInterval(interval);
-            resolve({
-              name: test.name!,
-              success: false,
-              error: `Walrus Test ${test.gid} timed out.`
-            });
-          }, 3600000);
-        });
-      }
-    )
-  );
+      return { name: test.name, success: false, error: error };
+    }
+  }));
 }
