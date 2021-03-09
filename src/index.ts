db3de8c53fa627e402486fe7406dc64cadc5d881
Akshay Nathan <akshay@walrus.ai>
2021-03-08 19:25:26 -0700

    Add ability to run tests by name from CLI

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,31 @@ const args = yargs
   .options({
     'api-key': { type: 'string', demandOption: true, alias: 'a' },
     url: { type: 'string', demandOption: false, alias: 'u' },
-    name: { type: 'string', demandOption: false, alias: 'n' },
+    name: { type: 'array', demandOption: false, alias: 'n' },
     revision: { type: 'string', demandOption: false, alias: 'r' },
     instructions: { type: 'array', demandOption: false, alias: 'i' },
     file: { type: 'string', demandOption: false, alias: 'f' },
     verbose: { type: 'boolean', demandOption: false, alias: 'v' },
   })
   .check((argv) => {
-    if (!argv.file && !(argv.instructions && argv.url)) {
-      throw new Error(
-        'You must specify either a file or a directory of tests OR an inline name, url and instructions'
-      );
+    if (argv.file && !argv.name && !argv.instructions && !argv.url) {
+      return true;
     }
 
-    return true;
+    if (argv.name && argv.name.length === 1 && argv.instructions && argv.url && !argv.file) {
+      return true;
+    }
+
+    if (argv.name && argv.name.length > 1 && !argv.instructions && !argv.url && !argv.file) {
+      return true;
+    }
+
+    throw new Error(`
+      You must specify either:
+        - An inline name, url, and instructions
+        - The path to a file or directory of test YML files
+        - A list of test names already created in the Walrus dashboard
+    `);
   }).argv;
 
 if (args.verbose) {
@@ -37,40 +48,43 @@ if (args.verbose) {
 if (args.file && fs.lstatSync(args.file).isDirectory()) {
   glob('/**/*.{yml,yaml}', { root: args.file }, (_, matches) => {
     try {
-      reportTests(matches.map(parseFile), (tests) =>
-        runTests(tests, args['api-key'], args.revision)
-      );
+      reportTests(matches.map(parseFile), (tests) => runTests(tests, args['api-key'], args.revision));
     } catch (e) {
       logger.error(e.message);
     }
   });
 } else if (args.file && fs.lstatSync(args.file).isFile()) {
   try {
-    reportTests([parseFile(args.file)], (tests) =>
-      runTests(tests, args['api-key'], args.revision)
-    );
+    reportTests([parseFile(args.file)], (tests) => runTests(tests, args['api-key'], args.revision));
   } catch (e) {
     logger.error(e.message);
   }
 } else if (args.file) {
   glob(args.file, { root: process.cwd() }, (_, matches) => {
     try {
-      reportTests(matches.map(parseFile), (tests) =>
-        runTests(tests, args['api-key'], args.revision)
-      );
+      reportTests(matches.map(parseFile), (tests) => runTests(tests, args['api-key'], args.revision));
     } catch (e) {
       logger.error(e.message);
     }
   });
 } else {
-  reportTests(
-    [
-      {
-        name: args.name!,
-        url: args.url!,
-        instructions: args.instructions as string[],
-      }
-    ],
-    (tests) => runTests(tests, args['api-key'], args.revision)
-  );
+  if (args.name!.length > 1) {
+    reportTests(
+      args.name!.map((name) => {
+        return { name: name.toString() };
+      }),
+      (tests) => runTests(tests, args['api-key'], args.revision),
+    );
+  } else {
+    reportTests(
+      [
+        {
+          name: args.name![0].toString(),
+          url: args.url!,
+          instructions: args.instructions as string[],
+        },
+      ],
+      (tests) => runTests(tests, args['api-key'], args.revision),
+    );
+  }
 }
