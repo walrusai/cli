20549f51a3aef6b9ce5448c1b40f2b1d884882d0
Akshay Nathan <akshay@walrus.ai>
2021-03-30 10:33:17 -0700

    Fix single quotes in JSON

--- src/reporter/index.ts
+++ src/reporter/index.ts
@@ -6,7 +6,7 @@ import logger from '../logger';
 
 export async function reportTests(
   tests: WalrusTest[],
-  runner: (tests: WalrusTest[]) => Promise<WalrusTestExecution[]>
+  runner: (tests: WalrusTest[]) => Promise<WalrusTestExecution[]>,
 ): Promise<boolean> {
   const message = `Running ${tests.length} test${tests.length > 1 ? 's' : ''}`;
   let spinner;
@@ -23,19 +23,19 @@ export async function reportTests(
     spinner.stop();
   }
 
-  const successes = executions.filter(value => !value.error);
-  const failures = executions.filter(value => value.error);
+  const successes = executions.filter((value) => !value.error);
+  const failures = executions.filter((value) => value.error);
 
   logger.info(chalk.green(`Successes: ${successes.length}`));
 
   for (const execution of successes) {
-    logger.info('%o', execution);
+    logger.info(JSON.stringify(execution, null, 2));
   }
 
   logger.info(chalk.red(`Failures: ${failures.length}`));
 
   for (const execution of failures) {
-    logger.info('%o', execution);
+    logger.info(JSON.stringify(execution, null, 2));
   }
 
   if (failures.length !== 0) {
